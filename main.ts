ee5d636d603e7b05ab9171432934735654dea16c
YatoGang <103865059+YatoGang@users.noreply.github.com>
2022-05-02 08:21:57 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("")
-    hand = randint(1, 3)
-    if (hand == 1) {
+radio.onReceivedNumber(function (receivedNumber) {
+    you_3 = receivedNumber
+})
+input.onGesture(Gesture.Shake, function () {
+    Yo_3 = 0
+    you_3 = 0
+    Yo_3 = randint(1, 3)
+    if (Yo_3 == 1) {
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -9,7 +13,7 @@ input.onButtonPressed(Button.A, function () {
             . # # # .
             . . . . .
             `)
-    } else if (hand == 2) {
+    } else if (Yo_3 == 2) {
         basic.showLeds(`
             # # # # #
             # . . . #
@@ -26,9 +30,24 @@ input.onButtonPressed(Button.A, function () {
             # . . # #
             `)
     }
+    radio.sendNumber(Yo_3)
+    basic.pause(1000)
+    basic.clearScreen()
+    if (you_3 == 1 && Yo_3 == 2 || (you_3 == 2 && Yo_3 == 3 || you_3 == 3 && Yo_3 == 1)) {
+        basic.showString("GANASTE!")
+    } else if (Yo_3 == you_3) {
+        basic.showString("EMPATE!")
+    } else {
+        basic.showString("PERDISTE!")
+    }
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
-let hand = 0
-radio.setGroup(2)
+let Yo_3 = 0
+let you_3 = 0
+radio.setGroup(13)
+let count = 6
+for (let index = 0; index < 5; index++) {
+    count += -1
+    basic.showNumber(count)
+    basic.pause(500)
+}
+basic.showString("Pvp :v")
